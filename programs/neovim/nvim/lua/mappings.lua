6baa968366cbb1232b0205623ac984de7dbb74f0
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2022-09-05 17:40:29 -0400

    Remove duplicate neovim config

--- programs/neovim/nvim/lua/mappings.lua
+++
@@ -1,4 +0,0 @@
--- Autoformat
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
-local bufopts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>=b", function() vim.lsp.buf.formatting { async = true } end, bufopts)
