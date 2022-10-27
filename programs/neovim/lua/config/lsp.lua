739ebbafd475ea1f83a1352f62b0c91646bffd47
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2022-10-26 20:38:26 -0400

    Deprecation

--- programs/neovim/lua/config/lsp.lua
+++ programs/neovim/lua/config/lsp.lua
@@ -5,7 +5,7 @@ local on_attach = function(_, bufnr)
 end
 
 local servers = { "zls", "pyright", "clangd" }
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
