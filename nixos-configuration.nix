e25c68eeb1a129babc83dc43aaec4526e96df57d
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2023-02-27 20:28:35 -0500

    sshagent

--- nixos-configuration.nix
+++ nixos-configuration.nix
@@ -143,6 +143,8 @@
   };
   programs.zsh.enable = true;
 
+  programs.ssh.startAgent = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
